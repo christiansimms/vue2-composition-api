8f8cfb9eebaa5f56e229a77112ea46dfe351c2eb
杨鑫 <17600528675@163.com>
2022-10-02 10:57:20 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <button @click="refPlus">child plus</button>
+    <HelloComposition ref="comp" />
+  </div>
+</template>
+
+<script setup>
+import HelloComposition from './components/HelloComposition.vue'
+
+</script>
+<script>
+export default {
+  name: 'App',
+  comments: { HelloComposition },
+  mounted() {
+    console.log(this.$refs.comp)
+  },
+  methods: {
+    refPlus() {
+      this.$refs.comp.plusCount();
+    }
+  }
+
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloComposition.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>composition: {{count}} {{num}}<button @click="plusCount">plus</button> </div>
+</template>
+<script>
+import { ref, defineComponent,computed } from "@vue/composition-api"
+export default defineComponent({
+  name: 'HelloComposition',
+  setup() {
+    const count = ref(1);
+    const num = computed(() => count.value * 2)
+
+    function plusCount() {
+      count.value++
+    }
+
+    return {
+      num,
+      count,
+      plusCount
+    }
+  }
+})
+</script>
