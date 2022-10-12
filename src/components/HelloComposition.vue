7c6fdfec03ba3d7b3145d6078a219f16a8746183
Yang <1342039458@qq.com>
2022-10-12 04:14:30 +0000

    Update 4 files

--- src/components/HelloComposition.vue
+++ src/components/HelloComposition.vue
@@ -1,23 +1,26 @@
 <template>
-  <div>composition: {{count}} {{num}}<button @click="plusCount">plus</button> </div>
+  <div>
+    composition: {{ count }} {{ num }}<button @click="plusCount">plus</button>
+  </div>
 </template>
 <script>
-import { ref, defineComponent,computed } from "@vue/composition-api"
+import { ref, defineComponent, computed } from "@vue/composition-api";
 export default defineComponent({
-  name: 'HelloComposition',
+  name: "HelloComposition",
   setup() {
     const count = ref(1);
-    const num = computed(() => count.value * 2)
+    const num = computed(() => count.value * 2);
 
     function plusCount() {
-      count.value++
+      console.log(count);
+      count.value++;
     }
 
     return {
       num,
       count,
-      plusCount
-    }
-  }
-})
+      plusCount,
+    };
+  },
+});
 </script>
